cd3c730a7518fed0a415335a3962a7454a24c10b
attaryu <mattarannaufal@gmail.com>
2025-09-29 16:20:55 +0700

    http method

--- src/user/user/user.controller.ts
+++ src/user/user/user.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 
 @Controller('/api/user')
-export class UserController {}
+export class UserController {
+  @Post()
+  post(): string {
+    return 'POST';
+  }
+
+  @Get('/sample')
+  get(): string {
+    return 'GET';
+  }
+}
